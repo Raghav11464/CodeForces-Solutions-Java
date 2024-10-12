8f7e30e0a180baacd4f3fb490b600519c867bafd
Raghav Sharan Mishra <66465208+Raghav11464@users.noreply.github.com>
2024-10-12 11:24:00 +0530

    Update HelpfulMaths.java

--- HelpfulMaths.java
+++ HelpfulMaths.java
@@ -5,11 +5,11 @@ public class HelpfulMaths {
         String str;
         char temp;
         str=s.nextLine();
-        char[] ch=str.toCharArray();
+        char[] ch=str.toCharArray();        //String converted to array of characters
         for(int i=0;i<ch.length;i+=2){
             for(int j=0;j<ch.length-i-1;j+=2){
                 if(ch[j]>ch[j+2]){
-                    temp=ch[j];
+                    temp=ch[j];        //Using Bubble Sort algorithm
                     ch[j]=ch[j+2];
                     ch[j+2]=temp;
                 }
